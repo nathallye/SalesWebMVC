21180d67fb2e18c67b926a370fed0a4dd16e5e8f
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-06 22:03:54 -0300

    created controller departments

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department { Id = 1, Name = "Eletronics" }); // sintaxe de instânciação automática
+            departments.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(departments); // enviando dados do controller para a view
+        }
+    }
+}
--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SalesWebMVC.Controllers
