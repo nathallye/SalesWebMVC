02dce0f1adc96e64e984591117938af5a04182b5
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-07 20:27:04 -0300

    SellersController

---
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesWebMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        // possiveis status de uma venda
+        Pending = 0,
+        Billed = 1,
+        Canceled =2
+    }
+}
