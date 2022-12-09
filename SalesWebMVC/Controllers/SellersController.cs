832b67908d14a72188a90c7d6b5c6bd0d66a1f5d
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-08 22:53:19 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -35,6 +35,33 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); // nameof - para previnir caso essa view tenha o nome trocado não quebre o código
+        }
+
+        // Action Delete com o método get só para exibirmos a tela confirmação
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) 
+            { 
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // Action Delete com o método post para deletarmos de fato e redirecionar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 using System.Data;
@@ -23,6 +24,18 @@ namespace SalesWebMVC.Services
         {
             /*obj.Department = _context.Department.First();*/
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id) 
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id) 
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
