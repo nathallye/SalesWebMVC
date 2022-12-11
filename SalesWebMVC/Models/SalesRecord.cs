1500b4aa5eb89e3c78da0d2bd12fd1df94a022ce
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-10 21:44:41 -0300

    Implementing grouping search

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -1,12 +1,18 @@
 ﻿using SalesWebMVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+        
         public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
 
